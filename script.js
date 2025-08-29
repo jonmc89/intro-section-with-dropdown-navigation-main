d5ec9def6c52ddf38695daa34f7a636a81f86250
Jon <126274870+jonmc89@users.noreply.github.com>
2025-08-29 12:34:45 +0100

    feat(Hidden Nav) - JS functions finished for collapsing header.
    style(Hidden Nav, Open Menu Button & Close Menu Button) - Hover and Active states added.

--- script.js
+++ script.js
@@ -1,15 +1,66 @@
 console.log("Script Loaded");
+
+// Hidden Nav Menu
+
 const openMenuBtn = document.getElementById("menuBtn");
 const hiddenNav = document.getElementById("hiddenNav");
 const overlay = document.getElementById("overlay");
 const closeBtn = document.getElementById("closeBtn");
+
+// Feature Section of Nav Menu
+
+const featureSection = document.getElementById("featureSection");
+const featuresContainer = document.getElementById("featuresCollapse");
+const featureArrow = document.getElementById("featureArrow");
+
+// Company Section of Nav Menu
+
+const companySection = document.getElementById("companySection");
+const companyContainer = document.getElementById("companyCollapse");
+const companyArror = document.getElementById("companyArrow");
+
+// Opens Nav Menu
 
 openMenuBtn.addEventListener("click", () => {
   hiddenNav.classList.add("unhide-nav");
   overlay.classList.add("show-overlay");
 });
 
+// Closes Nav Menu
+
 closeBtn.addEventListener("click", () => {
   hiddenNav.classList.remove("unhide-nav");
   overlay.classList.remove("show-overlay");
+});
+
+// Collapse or Expand Feature Menu
+
+featureArrow.addEventListener("click", () => {
+  const isCollapsed = featuresContainer.classList.contains("feature-collapse");
+
+  if (isCollapsed) {
+    featureSection.classList.remove("collapse-features");
+    featuresContainer.classList.remove("feature-collapse");
+    featureArrow.classList.remove("arrow-rotate");
+  } else {
+    featureSection.classList.add("collapse-features");
+    featuresContainer.classList.add("feature-collapse");
+    featureArrow.classList.add("arrow-rotate");
+  }
+});
+
+// Collapse or Expand Company Menu
+
+companyArror.addEventListener("click", () => {
+  const isCollapsed = companyContainer.classList.contains("company-collapse");
+
+  if (isCollapsed) {
+    companySection.classList.remove("collapse-company");
+    companyContainer.classList.remove("company-collapse");
+    companyArror.classList.remove("arrow-rotate");
+  } else {
+    companySection.classList.add("collapse-company");
+    companyContainer.classList.add("company-collapse");
+    companyArror.classList.add("arrow-rotate");
+  }
 });
